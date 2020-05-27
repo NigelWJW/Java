56d5f18d8e3033740f2998e6f948d7197caf8bda
NigelWJW <1341213521@qq.com>
2020-05-27 14:14:26 +0800

    2020.5.24 更新设计模式之工厂模式

--- DesignPatterns/src/Factory/SimpleFactory/SimpleFactory.java
+++ DesignPatterns/src/Factory/SimpleFactory/SimpleFactory.java
@@ -1,6 +1,8 @@
 package Factory.SimpleFactory;
 /**
- *  披萨简单工厂，把创建披萨对象放在一个类中，这样我们新增披萨类就只用修改这里即可
+ * 披萨简单工厂，把创建披萨对象放在一个类中，这样我们新增披萨类就只用修改这里即可
+ * Calendar类使用了简单工厂类，Calendar的getInstance方法中的createCalendar方法
+ * 里面根据对象创建实例
  */
 public class SimpleFactory {
     public Pizza creatPizza(String orderType){//简单工厂类的创建函数
