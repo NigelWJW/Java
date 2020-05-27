0c981d59bd59e0e52f32c13d5b19c038dc3e512d
NigelWJW <1341213521@qq.com>
2020-05-27 17:07:30 +0800

    2020.5.27 更新设计模式之适配器模式

--- DesignPatterns/src/Adapter/objectadapter/Client.java
+++ DesignPatterns/src/Adapter/objectadapter/Client.java
@@ -3,8 +3,8 @@ package Adapter.objectadapter;
 /**
  * 对象适配器模式：使用组合替代继承关系，解决了局限性的问题，而且目标类也不一定是接口了
  * 更灵活
- * SpringMVC的HandleAdapter使用了适配器，DispatchServlet的doDispatch方法接收了
- * request，使用controller接口获取HandleAdapter接口，HandleAdapter根据不同的请求在
+ * SpringMVC的HandleAdapter使用了适配器，Controller端的DispatchServlet的doDispatch方法接收了
+ * request，Model端使用controller接口获取HandleAdapter接口，HandleAdapter根据不同的请求在
  * 匹配SimpleHandleAdapter\HTTPHandleAdapter\AnnotationHandleAdapter对用的controller
  * HTTPController、SimpleController、AnnotationController最后返回给View端
  */
