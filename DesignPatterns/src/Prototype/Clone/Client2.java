6d65547eef100b758b40425089f4d096915c0355
NigelWJW <1341213521@qq.com>
2020-05-27 14:15:52 +0800

    2020.5.24 更新设计模式之原型模式

--- DesignPatterns/src/Prototype/Clone/Client2.java
+++ DesignPatterns/src/Prototype/Clone/Client2.java
@@ -2,7 +2,7 @@ package Prototype.Clone;
 
 /**
  * 原型模式，更好的效率和扩展性，动态克隆，不用重新初始化对象，但是因为每个原型类都要重写clone方法，会修改源代码破坏OCP原则
- * spring的bean - getBean() - doGetBean()中用到了原型模式
+ * spring的bean - getBean() - doGetBean() 中的isProtoType方法中用到了原型模式
  */
 public class Client2 {
     public static void main(String[] args) {
