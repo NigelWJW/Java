41897afbc8cdb2dccf1f7063565c297bf48c3393
NigelWJW <1341213521@qq.com>
2020-05-30 13:08:05 +0800

    2020.5.30 更新设计模式之状态模式

--- DesignPatterns/src/state/Raffle/Client.java
+++ DesignPatterns/src/state/Raffle/Client.java
@@ -2,6 +2,8 @@ package state.Raffle;
 
 /**
  * 状态模式测试类-抽奖活动
+ * 状态模式：戴拿有很强的可读性，将每个状态的行为封装在对应的一个类中。而且方便维护，替代if else。符合开闭原则
+ * 方便增删，但是可能状态过多会产生很多类，加大维护难度
  */
 public class Client {
     public static void main(String[] args) {
